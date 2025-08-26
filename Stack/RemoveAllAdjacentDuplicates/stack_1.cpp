d0250ba64bd8e18190e0138b3a824a5ab2675243
IllyaGY <igordstudy12@gmail.com>
2025-08-25 22:18:17 -0700

    Remove All Adjacent Duplicates

---
+++ Stack/RemoveAllAdjacentDuplicates/stack_1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st; 
+        
+        for(char i : s){
+            if(!st.empty()&&st.top() == i) st.pop(); 
+            else st.push(i);
+        }
+        string res = ""; 
+        while(!st.empty()){
+            res+=st.top(); 
+            st.pop();
+        }        
+        reverse(res.begin(), res.end());
+        return res; 
+    }
+};
