cecabf441d15cb0469502f1d9d71df46ebe0f112
IllyaGY <igordstudy12@gmail.com>
2025-09-14 23:10:19 -0700

    [200~131. Palindrome Partitioning

---
+++ Backtracking/PalindromePartitioning/palindromePart.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void helper(vector<vector<string>> &res, const string& s, int start = 0){
+        static vector<string> v = {}; 
+        if(start == s.size()) res.push_back(v); 
+        int j = start; 
+        string st = "";
+        while(j < s.size()){ 
+            int i = start;
+            int k = j;
+            
+            st+=s[j]; 
+            while(i < k){
+                if(s[i] == s[k]){
+                    i++;
+                    k--;
+                }
+                else break; 
+            }     
+            if(i>=k){
+                v.push_back(st);
+                helper(res, s, j+1);
+                v.pop_back(); 
+            }
+            j++; 
+        } 
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res; 
+        if(s.size())helper(res, s);
+        return res; 
+    }
+};
+
