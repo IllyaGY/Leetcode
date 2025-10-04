8e005cdae2ca3bd662bbabd4bedb9029f90f0e8a
IllyaGY <igordstudy12@gmail.com>
2025-10-04 10:03:46 -0700

    113. Path Sum II

---
+++ Tree/PathSum2/pathSum2.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, int targetSum, vector<int>& summed, vector<vector<int>>& result){
+        if(!root || abs(targetSum) < 0) return; 
+        if(!root->left && !root->right){
+            if(!targetSum) result.push_back(summed); 
+            return; 
+        }
+        if(root->left) {
+            summed.push_back(root->left->val); 
+            helper(root->left, targetSum-root->left->val, summed, result); 
+            summed.pop_back(); 
+        }
+        if(root->right) {
+            summed.push_back(root->right->val); 
+            helper(root->right, targetSum-root->right->val, summed, result); 
+            summed.pop_back(); 
+        }
+    
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> result; 
+        vector<int> summed; 
+        if(root){
+            summed.push_back(root->val); 
+            helper(root, targetSum-root->val,summed, result); 
+            summed.pop_back(); 
+        }
+        return result; 
+    }
+};
