56df1c707143724c6e740b6150d1691c7ffe0ca9
IllyaGY <igordstudy12@gmail.com>
2025-12-17 18:55:06 -0800

    347. Top K Frequent Elements

---
+++ PriorityQueue/TopKFrequentElements/topKFreqEl-practice.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int, int> , vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        unordered_map<int, int> m; 
+        for(auto &i : nums) m[i]++; 
+        for(auto &i : m){  pq.push({i.second, i.first}); if(pq.size() > k) pq.pop();}; 
+        vector<int> res;
+        while(!pq.empty()) {
+            res.push_back(pq.top().second); 
+            pq.pop(); 
+        } 
+    
+        return res; 
+    }
+};
